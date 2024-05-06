7dd1af1d4f0cc65df143ee52fd743f123725cab2
PimehT <tareerepimeh@gmail.com>
2024-05-06 10:23:26 +0100

    trying again

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -79,10 +79,48 @@ def update_place(place_id):
 
 
 @app_views.route('/places_search', methods=['POST'], strict_slashes=False)
-def search_places():
-    """Method that retrieves all Place objects depending of
-    the JSON in the body of the request
+def places_search():
     """
+    Retrieves all Place objects depending of the JSON in
+    the body of the request.
+    """
+    # data = request.get_json(silent=True)
+    # if not isinstance(data, dict):
+    #     return jsonify({"error": "Not a JSON"}), 400
+
+    # states = data.get('states', [])
+    # cities = data.get('cities', [])
+    # amenities = data.get('amenities', [])
+
+    # if not states and not cities and not amenities:
+    #     places = storage.all(Place).values()
+    # else:
+    #     places = []
+    #     for state_id in states:
+    #         state = storage.get(State, state_id)
+    #         if state:
+    #             for city in state.cities:
+    #                 places.extend(city.places)
+    #     for city_id in cities:
+    #         city = storage.get(City, city_id)
+    #         if city:
+    #             # ensure no duplicates
+    #             for place in city.places:
+    #                 if place.id not in (place.id for place in places):
+    #                     places.append(place)
+
+    # # filter by amenity
+    # if len(amenities) > 0:
+    #     place_list = []
+    #     for place in places:
+    #         for place_amenity in place.amenities:
+    #             if place_amenity.id in amenities:
+    #                 place_list.append(place.to_dict())
+    #                 break
+    # else:
+    #     place_list = [place.to_dict() for place in places]
+
+    # return jsonify(place_list), 200
     if not request.is_json:
         return jsonify({"error": "Not a JSON"}), 400
 
@@ -126,47 +164,3 @@ def search_places():
         p_dict.pop('amenities', None)
         result.append(p_dict)
     return jsonify(result), 200
-
-# def places_search():
-#     """
-#     Retrieves all Place objects depending of the JSON in
-#     the body of the request.
-#     """
-#     data = request.get_json(silent=True)
-#     if not isinstance(data, dict):
-#         return jsonify({"error": "Not a JSON"}), 400
-
-#     states = data.get('states', [])
-#     cities = data.get('cities', [])
-#     amenities = data.get('amenities', [])
-
-#     if not data or (not states and not cities and not amenities):
-#         place_list = [place.to_dict() for place in storage.all(Place).values()]
-#         return jsonify(place_list), 200
-#     else:
-#         places = []
-#         for state_id in states:
-#             state = storage.get(State, state_id)
-#             if state:
-#                 for city in state.cities:
-#                     places.extend(city.places)
-#         for city_id in cities:
-#             city = storage.get(City, city_id)
-#             if city:
-#                 # ensure no duplicates
-#                 for place in city.places:
-#                     if place.id not in (place.id for place in places):
-#                         places.append(place)
-
-#     # filter by amenity
-#     if len(amenities) > 0:
-#         place_list = []
-#         for place in places:
-#             for place_amenity in place.amenities:
-#                 if place_amenity.id in amenities:
-#                     place_list.append(place.to_dict())
-#                     break
-#     else:
-#         place_list = [place.to_dict() for place in places]
-
-#     return jsonify(place_list), 200
